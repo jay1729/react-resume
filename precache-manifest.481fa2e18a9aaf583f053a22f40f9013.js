7e5bcd096c963eaa38987352ff032bac0528a84b
Jay Gumballi <jayantha.gumballi@gmail.com>
2019-03-11 23:38:45 +0530

    Updates

---
+++ precache-manifest.481fa2e18a9aaf583f053a22f40f9013.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4a4f1d4068793d41daa0367a4ab60a3",
+    "url": "/react-resume/static/media/envelope.d4a4f1d4.svg"
+  },
+  {
+    "revision": "505a6158b1011ad7ccb5",
+    "url": "/react-resume/static/css/main.9f551553.chunk.css"
+  },
+  {
+    "revision": "9ce08e71c00fa3755713",
+    "url": "/react-resume/static/js/runtime~main.9ce08e71.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "07e4b7b1dc53b8a717b7",
+    "url": "/react-resume/static/js/2.07e4b7b1.chunk.js"
+  },
+  {
+    "revision": "f137fcb528192baca59d48f5d1f28c64",
+    "url": "/react-resume/static/media/github.f137fcb5.svg"
+  },
+  {
+    "revision": "48a2133dd5c34ad46cc6ab6ff06c03ec",
+    "url": "/react-resume/static/media/linkedin.48a2133d.svg"
+  },
+  {
+    "revision": "505a6158b1011ad7ccb5",
+    "url": "/react-resume/static/js/main.505a6158.chunk.js"
+  },
+  {
+    "revision": "b4a792a2e1c25870a11fb8c72b92bd24",
+    "url": "/react-resume/static/media/header-backround.b4a792a2.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-resume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "07e4b7b1dc53b8a717b7",
+    "url": "/react-resume/static/css/2.3905556e.chunk.css"
+  },
+  {
+    "revision": "acbaa38d9a6afd70698ed257e00dabc2",
+    "url": "/react-resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17541bd1be271e643b7d0359ca4359b6.js"
+  "/react-resume/precache-manifest.481fa2e18a9aaf583f053a22f40f9013.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
